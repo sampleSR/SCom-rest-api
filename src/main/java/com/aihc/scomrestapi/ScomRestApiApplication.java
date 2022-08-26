1afafd61e6b53f3910f105604ae6fda897d5f6fb
Alvaro-IHC <alva0741@gmail.com>
2022-08-26 18:53:54 -0400

    Initial commit

---
+++ src/main/java/com/aihc/scomrestapi/ScomRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.aihc.scomrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScomRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScomRestApiApplication.class, args);
+	}
+
+}
