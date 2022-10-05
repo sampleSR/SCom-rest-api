82500faf48bb128aee307ef00bf0d22c0bf8874a
Israel <alva0741@gmail.com>
2022-10-05 17:05:35 -0400

    Added: PATCH method in CORS config

--- src/main/java/com/aihc/scomrestapi/config/WebConfig.java
+++ src/main/java/com/aihc/scomrestapi/config/WebConfig.java
@@ -11,6 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");
+    registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE",
+        "PATCH");
   }
 }
