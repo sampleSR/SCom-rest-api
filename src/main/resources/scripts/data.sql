37946b0e08ef1bfa945ebe7e45b08686f17e1a01
Israel <alva0741@gmail.com>
2022-10-12 19:26:28 -0400

    Modified: SQL script and product model

--- src/main/resources/scripts/data.sql
+++ src/main/resources/scripts/data.sql
@@ -1,20 +1,49 @@
 -- Users
+-- Admins
 INSERT IGNORE INTO usuario (id_u, username, ap_paterno, ap_materno, nombre, email, contrasenia, direccion) VALUES (1, 'aihc', 'Huanca', 'Condori', 'Alvaro', 'alva0741@gmail.com', 'alvaro123', 'Zona: zona, Calle: calle');
 INSERT IGNORE INTO administrador (id_u, salario, especialidad) VALUES (1, 40000, 'Admin');
-
+-- Cashiers
 INSERT IGNORE INTO usuario (id_u, username, ap_paterno, ap_materno, nombre, email, contrasenia, direccion) VALUES (2, 'cashier', '', '', 'Cashier', 'cashier@gmail.com', 'cashier', 'Zona: zona, Calle: calle');
 INSERT IGNORE INTO cajero (id_u, salario, titulo) VALUES (2, 20000, 'lic');
-
+-- Chefs
 INSERT IGNORE INTO usuario (id_u, username, ap_paterno, ap_materno, nombre, email, contrasenia, direccion) VALUES (3, 'chef', '', '', 'chef', 'chef@gmail.com', 'chef', 'Zona: zona, Calle: calle');
 INSERT IGNORE INTO chef (id_u, salario, especialidad, nacionalidad, cod_sanidad) VALUES (3, 20000, 'local', 'Boliviano', '15051ADC');
+-- Waiters
+INSERT IGNORE  INTO usuario (id_u, direccion, email, ap_paterno, ap_materno, nombre, contrasenia,
+                             username) VALUES (16, 'Zona: zona, Calle: calle', 'Liz@gmail.com',
+                                               'choque', 'mamani', 'liz', 'waiter', 'waiter');
+INSERT IGNORE INTO camarero(experiencia, cod_sanidad, salario, id_u) VALUES (3, 1875, 2030, 16);
+INSERT IGNORE  INTO usuario (id_u, direccion, email, ap_paterno, ap_materno, nombre, contrasenia,
+                             username) VALUES (17, 'Zona: zona, Calle: calle', 'fidel@gmail.com',
+                                             'Ramos', 'quispe', 'Fidel', 'waiter2', 'waiter2');
+INSERT IGNORE INTO camarero(experiencia, cod_sanidad, salario, id_u) VALUES (3, 7845, 2030, 17);
+INSERT IGNORE  INTO usuario (id_u, direccion, email, ap_paterno, ap_materno, nombre, contrasenia,
+                             username) VALUES (18, 'Zona: zona, Calle: calle', 'freddy@gmail.com',
+                                             'condori', 'mamani', 'Freddy', 'waiter3', 'waiter3');
+INSERT IGNORE INTO camarero(experiencia, cod_sanidad, salario, id_u) VALUES (4, 2254, 2030, 18);
+INSERT IGNORE  INTO usuario (id_u, direccion, email, ap_paterno, ap_materno, nombre, contrasenia,
+                             username) VALUES (19, 'Zona: zona, Calle: calle', 'marta@gmail.com',
+                                             'Colque', 'flores', 'Marta', 'waiter4', 'waiter4');
+INSERT IGNORE INTO camarero(experiencia, cod_sanidad, salario, id_u) VALUES (2, 8045, 2030, 19);
+INSERT IGNORE  INTO usuario (id_u, direccion, email, ap_paterno, ap_materno, nombre, contrasenia,
+                             username) VALUES (20, 'Zona: zona, Calle: calle', 'norma@gmail.com',
+                                             'Mendoza', 'aliaga', 'Norma', 'waiter5', 'waiter5');
+INSERT IGNORE INTO camarero(experiencia, cod_sanidad, salario, id_u) VALUES (1, 7369, 2030, 20);
+
+-- Waiter >-< Table Relationship
+INSERT IGNORE INTO atiende(id_camarero, id_mesa) VALUES (16, 1);
+INSERT IGNORE INTO atiende(id_camarero, id_mesa) VALUES (17, 2);
+INSERT IGNORE INTO atiende(id_camarero, id_mesa) VALUES (18, 3);
+INSERT IGNORE INTO atiende(id_camarero, id_mesa) VALUES (19, 4);
+INSERT IGNORE INTO atiende(id_camarero, id_mesa) VALUES (20, 5);
 
 -- Images
-INSERT IGNORE INTO scom_rest.imagen (id_i, nombre, public_id, url) VALUES (1, 'plato1', 'b3invzperhdljka3xbzw', 'http://res.cloudinary.com/dcm0rx3pb/image/upload/v1664934151/b3invzperhdljka3xbzw.jpg');
-INSERT IGNORE INTO scom_rest.imagen (id_i, nombre, public_id, url) VALUES (2, 'plato2', 'u7vtvq6txfafxg09gseh', 'http://res.cloudinary.com/dcm0rx3pb/image/upload/v1664934208/u7vtvq6txfafxg09gseh.png');
-INSERT IGNORE INTO scom_rest.imagen (id_i, nombre, public_id, url) VALUES (3, 'plato3', 'q7qipy0xjk0j8irk11dr', 'http://res.cloudinary.com/dcm0rx3pb/image/upload/v1664934222/q7qipy0xjk0j8irk11dr.jpg');
-INSERT IGNORE INTO scom_rest.imagen (id_i, nombre, public_id, url) VALUES (4, 'bebida1', 'sevgcx66due0mfpzy6le', 'http://res.cloudinary.com/dcm0rx3pb/image/upload/v1664934233/sevgcx66due0mfpzy6le.jpg');
-INSERT IGNORE INTO scom_rest.imagen (id_i, nombre, public_id, url) VALUES (5, 'bebida2', 'rj6k9bqiol16qa6gzagf', 'http://res.cloudinary.com/dcm0rx3pb/image/upload/v1664934242/rj6k9bqiol16qa6gzagf.jpg');
-INSERT IGNORE INTO scom_rest.imagen (id_i, nombre, public_id, url) VALUES (6, 'bebida3', 'okjtdtunxowxjjmhr5yy', 'http://res.cloudinary.com/dcm0rx3pb/image/upload/v1664934252/okjtdtunxowxjjmhr5yy.jpg');
+INSERT IGNORE INTO imagen (id_i, nombre, public_id, url) VALUES (1, 'plato1', 'b3invzperhdljka3xbzw', 'http://res.cloudinary.com/dcm0rx3pb/image/upload/v1664934151/b3invzperhdljka3xbzw.jpg');
+INSERT IGNORE INTO imagen (id_i, nombre, public_id, url) VALUES (2, 'plato2', 'u7vtvq6txfafxg09gseh', 'http://res.cloudinary.com/dcm0rx3pb/image/upload/v1664934208/u7vtvq6txfafxg09gseh.png');
+INSERT IGNORE INTO imagen (id_i, nombre, public_id, url) VALUES (3, 'plato3', 'q7qipy0xjk0j8irk11dr', 'http://res.cloudinary.com/dcm0rx3pb/image/upload/v1664934222/q7qipy0xjk0j8irk11dr.jpg');
+INSERT IGNORE INTO imagen (id_i, nombre, public_id, url) VALUES (4, 'bebida1', 'sevgcx66due0mfpzy6le', 'http://res.cloudinary.com/dcm0rx3pb/image/upload/v1664934233/sevgcx66due0mfpzy6le.jpg');
+INSERT IGNORE INTO imagen (id_i, nombre, public_id, url) VALUES (5, 'bebida2', 'rj6k9bqiol16qa6gzagf', 'http://res.cloudinary.com/dcm0rx3pb/image/upload/v1664934242/rj6k9bqiol16qa6gzagf.jpg');
+INSERT IGNORE INTO imagen (id_i, nombre, public_id, url) VALUES (6, 'bebida3', 'okjtdtunxowxjjmhr5yy', 'http://res.cloudinary.com/dcm0rx3pb/image/upload/v1664934252/okjtdtunxowxjjmhr5yy.jpg');
 
 -- Foods
 INSERT IGNORE INTO producto (id_p, disponible, precio, id_imagen) VALUES (1, true, 25.0, 1);
@@ -58,44 +87,51 @@ INSERT IGNORE INTO factura (id_f, id_cajero, id_cliente) VALUES (2, 2, 10);
 INSERT IGNORE INTO factura (id_f, id_cajero, id_cliente) VALUES (3, 2, 11);
 INSERT IGNORE INTO factura (id_f, id_cajero, id_cliente) VALUES (4, 2, 11);
 INSERT IGNORE INTO factura (id_f, id_cajero, id_cliente) VALUES (5, 2, 12);
-INSERT IGNORE INTO factura (id_f, id_cajero, id_cliente) VALUES (6, 2, 13);
-INSERT IGNORE INTO factura (id_f, id_cajero, id_cliente) VALUES (7, 2, 13);
-INSERT IGNORE INTO factura (id_f, id_cajero, id_cliente) VALUES (8, 2, 13);
-INSERT IGNORE INTO factura (id_f, id_cajero, id_cliente) VALUES (9, 2, 14);
-INSERT IGNORE INTO factura (id_f, id_cajero, id_cliente) VALUES (10, 2, 14);
 
 -- Restaurant Tables
-INSERT IGNORE INTO mesa(id_m, capacidad, numero) VALUES (1, 4, '1');
-INSERT IGNORE INTO mesa(id_m, capacidad, numero) VALUES (2, 2, '2');
-INSERT IGNORE INTO mesa(id_m, capacidad, numero) VALUES (3, 4, '3');
-INSERT IGNORE INTO mesa(id_m, capacidad, numero) VALUES (4, 6, '4');
-INSERT IGNORE INTO mesa(id_m, capacidad, numero) VALUES (5, 6, '5');
+INSERT IGNORE INTO mesa (id_m, capacidad, numero) VALUES (1, 4, '1');
+INSERT IGNORE INTO mesa (id_m, capacidad, numero) VALUES (2, 2, '2');
+INSERT IGNORE INTO mesa (id_m, capacidad, numero) VALUES (3, 4, '3');
+INSERT IGNORE INTO mesa (id_m, capacidad, numero) VALUES (4, 6, '4');
+INSERT IGNORE INTO mesa (id_m, capacidad, numero) VALUES (5, 6, '5');
 
 -- Orders
-INSERT IGNORE INTO scom_rest.pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
-                               id_factura, id_chef, id_cliente, id_mesa) VALUES (1, '2008-05-31 20:15:50', '2008-05-31 22:06:50', false, false, false, 1, 3, 10, 1);
-INSERT IGNORE INTO scom_rest.pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
-                               id_factura, id_chef, id_cliente, id_mesa) VALUES (3, '2008-06-16 20:06:50', '2008-06-16 20:45:50', false, false, false, 3, 3, 11, 3);
-INSERT IGNORE INTO scom_rest.pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
-                               id_factura, id_chef, id_cliente, id_mesa) VALUES (4, '2015-02-22 20:06:50', '2015-02-22 20:36:50', false, false, false, 4, 3, 11, 2);
-INSERT IGNORE INTO scom_rest.pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
-                               id_factura, id_chef, id_cliente, id_mesa) VALUES (5, '2015-07-14 20:06:50', '2015-07-14 20:15:50', false, false, false, 5, 3, 12, 2);
-INSERT IGNORE INTO scom_rest.pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
-                               id_factura, id_chef, id_cliente, id_mesa) VALUES (6, '2020-07-18 20:06:50', '2020-07-18 20:18:50', false, false, false, 6, 3, 11, 1);
-INSERT IGNORE INTO scom_rest.pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
-                               id_factura, id_chef, id_cliente, id_mesa) VALUES (7, '2020-11-30 20:06:50', '2020-11-30 20:41:50', false, false, false, 7, 3, 13, 3);
-INSERT IGNORE INTO scom_rest.pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
-                               id_factura, id_chef, id_cliente, id_mesa) VALUES (8, '2020-01-05 20:06:50', '2020-01-05 20:18:50', false, false, false, 8, 3, 14, 4);
-INSERT IGNORE INTO scom_rest.pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
-                               id_factura, id_chef, id_cliente, id_mesa) VALUES (9, '2022-07-31 20:06:50', '2022-07-31 20:36:50', false, false, false, 8, 3, 10, 5);
-INSERT IGNORE INTO scom_rest.pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
-                               id_factura, id_chef, id_cliente, id_mesa) VALUES (10, '2022-08-02 20:06:50', '2022-08-02 20:45:50', false, false, false, 9, 3, 13, 4);
+INSERT IGNORE INTO pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
+                               id_factura, id_chef, id_cliente, id_mesa) VALUES (1, '2008-05-31
+20:15:50', '2008-05-31 22:06:50', true, true, true, 1, 3, 10, 1);
+INSERT IGNORE INTO pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
+                               id_factura, id_chef, id_cliente, id_mesa) VALUES (2, '2008-03-24
+20:15:50', '2008-03-24 22:06:50', true, true, true, 2, 3, 10, 1);
+INSERT IGNORE INTO pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
+                               id_factura, id_chef, id_cliente, id_mesa) VALUES (3, '2008-06-16
+20:06:50', '2008-06-16 20:45:50', true, true, true, 3, 3, 11, 3);
+INSERT IGNORE INTO pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
+                               id_factura, id_chef, id_cliente, id_mesa) VALUES (4, '2015-02-22
+20:06:50', '2015-02-22 20:36:50', true, true, true, 4, 3, 11, 2);
+INSERT IGNORE INTO pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
+                               id_factura, id_chef, id_cliente, id_mesa) VALUES (5, '2015-07-14
+20:06:50', '2015-07-14 20:15:50', true, true, true, 5, 3, 12, 2);
+INSERT IGNORE INTO pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
+                               id_factura, id_chef, id_cliente, id_mesa) VALUES (6, '2020-07-18
+20:06:50', null, false, false, false, null, null, 11, 1);
+INSERT IGNORE INTO pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
+                               id_factura, id_chef, id_cliente, id_mesa) VALUES (7, '2020-11-30
+20:06:50', null, false, false, false, null, null, 13, 3);
+INSERT IGNORE INTO pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
+                               id_factura, id_chef, id_cliente, id_mesa) VALUES (8, '2020-01-05
+20:06:50', null, false, false, false, null, null, 14, 4);
+INSERT IGNORE INTO pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
+                               id_factura, id_chef, id_cliente, id_mesa) VALUES (9, '2022-07-31
+20:06:50', null, false, false, false, null, null, 10, 5);
+INSERT IGNORE INTO pedido (id_pe, fecha, fecha_factura, entregado, preparado, confirmado,
+                               id_factura, id_chef, id_cliente, id_mesa) VALUES (10, '2022-08-02
+20:06:50', null, false, false, false, null, null, 13, 4);
 
 -- Order >-< Product Relationship
 INSERT IGNORE INTO tiene (id_pedido, id_producto, cantidad) VALUES (1, 1, 2);
 INSERT IGNORE INTO tiene (id_pedido, id_producto, cantidad) VALUES (1, 2, 4);
--- INSERT IGNORE INTO tiene (id_pedido, id_producto, cantidad) VALUES (2, 2, 3);
--- INSERT IGNORE INTO tiene (id_pedido, id_producto, cantidad) VALUES (2, 3, 1);
+INSERT IGNORE INTO tiene (id_pedido, id_producto, cantidad) VALUES (2, 2, 3);
+INSERT IGNORE INTO tiene (id_pedido, id_producto, cantidad) VALUES (2, 3, 1);
 INSERT IGNORE INTO tiene (id_pedido, id_producto, cantidad) VALUES (3, 1, 2);
 INSERT IGNORE INTO tiene (id_pedido, id_producto, cantidad) VALUES (4, 4, 2);
 INSERT IGNORE INTO tiene (id_pedido, id_producto, cantidad) VALUES (4, 5, 3);
